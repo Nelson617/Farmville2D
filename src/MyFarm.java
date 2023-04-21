7583832ff9a5f8217c0f3fb7cbcd7df244cdc2b6
nelson <ralph_nelson25@milton.edu>
2023-04-21 13:05:27 -0400

    Initial commit

---
+++ src/MyFarm.java
@@ -0,0 +1,10 @@
+public class MyFarm {
+    public static void main(String[] arge) {
+        MyFarm charlie = new MyFarm();
+    }
+
+
+    public MyFarm(){
+        System.out.println("Hello from my farm!");
+    }
+}
