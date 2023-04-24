87f4cad06fd1022206a3f8d497a58da24ccd320d
nelson <ralph_nelson25@milton.edu>
2023-04-24 14:07:24 -0400

    i added to my farm

--- src/MyFarm.java
+++ src/MyFarm.java
@@ -4,7 +4,19 @@ public class MyFarm {
     }
 
 
-    public MyFarm(){
+    public MyFarm() {
         System.out.println("Hello from my farm!");
+        Plot Nelson = new Plot();
+        Nelson.printPlot();
+
+        Plot[][] grid = new Plot[100][50];
+
+        for (int n = 1; n < grid.length; n++) {
+            for (int r = 1; r < grid[n].length; r++) {
+                grid[n][r] =new Plot();
+                grid[n][r].printPlot();
+            }
+        }
     }
 }
+
