caa575b8f9dc7fa0356384e939a8972831448010
nelson <ralph_nelson25@milton.edu>
2023-04-28 13:05:17 -0400

    total plants and printed plant names

--- src/MyFarm.java
+++ src/MyFarm.java
@@ -3,20 +3,57 @@ public class MyFarm {
         MyFarm charlie = new MyFarm();
     }
 
+    public Plot[][] grid;
 
     public MyFarm() {
         System.out.println("Hello from my farm!");
         Plot Nelson = new Plot();
         Nelson.printPlot();
 
-        Plot[][] grid = new Plot[100][50];
+//        Nelson.NumberOfPlant;
 
-        for (int n = 1; n < grid.length; n++) {
-            for (int r = 1; r < grid[n].length; r++) {
-                grid[n][r] =new Plot();
+        grid = new Plot[4][5];
+
+        for (int n = 0; n < grid.length; n++) {
+            for (int r = 0; r < grid[n].length; r++) {
+                grid[n][r] = new Plot();
                 grid[n][r].printPlot();
             }
         }
+
+
+        //call methods here
+        totalPlants();
+        printPlantNames();
+
+
+    }
+
+    public void totalPlants() {
+        //how many total plants are there in the grid?
+        int sumOfPlants = 0;
+        for (int e = 0; e < grid.length; e++) {
+            for (int c = 0; c < grid[e].length; c++) {
+                sumOfPlants += grid[e][c].NumberOfPlants;
+
+
+            }
+        }
+        System.out.println(sumOfPlants);
+
+    }
+
+    public void printPlantNames() {
+        // print the name of each plant in each row and column
+        // row: 0 col: 0 corn
+        for (int r = 0; r < grid.length; r++) {
+            for (int c = 0; c < grid[r].length; c++) {
+                System.out.println("row: " + r + " col: " + c + " " + grid[r][c].PlantName);
+            }
+
+
+        }
+
     }
 }
 
