16a01a91573bd974d50e6d0ed502f32eef375da5
nelson <ralph_nelson25@milton.edu>
2023-05-08 14:13:21 -0400

    max numbers

--- src/MyFarm.java
+++ src/MyFarm.java
@@ -27,6 +27,8 @@ public class MyFarm {
         printPlantNames();
         totalCarrots();
         averageNumberOfPlants();
+        plotWithMaxNumber();
+        plantWithMaxNumber();
 
     }
 
@@ -69,15 +71,84 @@ public class MyFarm {
 
     public void averageNumberOfPlants() {
         // what is the average number of plants across the whole row?
-            int numOfPlants = 0;
-            int totalPlants = 0;
-            for (int z = 0; z < grid.length; z++) {
-                for (int g = 0; g < grid[z].length; g++) {
-                    numOfPlants += grid [z][g].NumberOfPlants;
-                    totalPlants = totalPlants +1 ;
+        int numOfPlants = 0;
+        int totalPlants = 0;
+        for (int z = 0; z < grid.length; z++) {
+            for (int g = 0; g < grid[z].length; g++) {
+                numOfPlants += grid[z][g].NumberOfPlants;
+                totalPlants = totalPlants + 1;
+            }
+        }
+        System.out.println("the average number of plants is " + numOfPlants / totalPlants);
+    }
+
+    public void plotWithMaxNumber() {
+        // which plot type has the most total plants?
+        int maxNumber = grid[0][0].NumberOfPlants;
+        String name = grid[0][0].PlantName;
+        for (int b = 0; b < grid.length; b++) {
+            for (int g = 0; g > grid[b].length; g++) {
+                if (grid[b][g].NumberOfPlants > maxNumber) {
+                    maxNumber = grid[b][g].NumberOfPlants;
+                    name = grid[b][g].PlantName;
+
                 }
             }
-        System.out.println("the average number of plants is" + numOfPlants/totalPlants);
+        }
+        System.out.println(name + "has the most plants with a total of " + maxNumber + " plants");
+    }
+
+    public void plantWithMaxNumber() {
+        int numCorn = 0;
+        int numTomato = 0;
+        int numCarrot = 0;
+        int numSunflower = 0;
+
+        // which plant type has the most total plants?
+        for (int b = 0; b < grid.length; b++) {
+            for (int g = 0; g > grid[b].length; g++) {
+                switch(grid[b][g].PlantName){
+                    case "corn":
+                        numCorn += grid[b][g].NumberOfPlants;
+                        break;
+                    case "snuflower":
+                        numSunflower += grid[b][g].NumberOfPlants;
+                        break;
+                    case "Tomato":
+                        numTomato += grid[b][g].NumberOfPlants;
+                        break;
+                    default:
+                        numCarrot += grid[b][g].NumberOfPlants;
+                    break;
+                }
+                if(grid[b][g].PlantName.equals("corn")){
+
+                }
+            }
+        }
+
+        if(numCorn > numCarrot && numCorn > numSunflower && numCarrot > numTomato) {
+            System.out.println("Corn has the most plants with " + numCorn + " plants");
+        }
+
+        if(numTomato > numCarrot && numTomato > numSunflower && numTomato > numCorn) {
+            System.out.println("Tomato has the most plants with " + numTomato + " plants");
+        }
+
+        if(numCarrot > numTomato && numCarrot > numSunflower && numCarrot > numCorn) {
+            System.out.println("Carrot has the most plants with " + numCarrot + " plants");
+        }
+        if(numSunflower > numCarrot && numSunflower > numTomato && numSunflower > numCorn) {
+            System.out.println("Sunflower has the most plants with " + numSunflower + " plants");
+        }
+        else{
+            System.out.println("idk");
+        }
+
     }
 }
+
+
+
+
 
